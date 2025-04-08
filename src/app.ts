ed0ffd0425171e34a15b753d9a6d7623080d6c53
Rasibul <rasibul179@gmail.com>
2025-04-08 10:30:24 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 
 const app: express.Application = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.send('Ph Health Care  Sever Is Running!');
