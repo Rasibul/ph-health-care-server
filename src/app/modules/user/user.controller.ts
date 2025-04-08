adf24339ba284c38d40ad0e4c3f5bf93c2bc62ba
Rasibul <rasibul179@gmail.com>
2025-04-09 00:13:03 +0600

    updated

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,7 +2,11 @@ import { Request, Response } from 'express';
 import { userService } from './user.service';
 const createAdmin = async (req: Request, res: Response) => {
     const result = await userService.createAdmin(req.body);
-    res.send(result);
+    res.status(200).json({
+        success: true,
+        message: 'Admin created successfully',
+        data: result,
+    });
 }
 
 export const userController = {
